ec79dabb29b0d378b72276d6d0918ba8d81fe8e9
Carsten Munk <carsten.munk@zippie.org>
2019-11-03 14:22:42 +0100

    Add base64 loader for svgs/etc
    Signed-off-by: Carsten Munk <carsten.munk@zippie.org>

--- craco-plugin.js
+++ craco-plugin.js
@@ -1,7 +1,19 @@
 
 const PolyfillIPFSScriptSrc = require('./PolyfillIPFSScriptSrc.js')
 const IpfsWebpackPlugin = require('./index.js')
+const base64Loader = require('craco-base64-inline-loader')
+
 module.exports = {
+    overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => { 
+      cracoConfig.plugins.push(
+      { 
+        plugin: base64Loader,
+        options: {
+          test: /\.(ttf|eot|otf|svg|woff(2)?)$/i,
+          limit: 99999999999999999999,
+        }
+      })
+    }
     overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env, paths } }) => {
       webpackConfig.plugins.unshift(new PolyfillIPFSScriptSrc())
       webpackConfig.plugins.push(new IpfsWebpackPlugin())
