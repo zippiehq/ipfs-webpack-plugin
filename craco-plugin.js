85eb336cfcaa61102607c71774d67e1cd37c519a
Carsten Munk <carsten.munk@zippie.org>
2019-11-25 08:13:03 +0300

    Fix bug with base64 inline loadeR
    Signed-off-by: Carsten Munk <carsten.munk@zippie.org>

--- craco-plugin.js
+++ craco-plugin.js
@@ -2,20 +2,21 @@
 const { loaderByName, addBeforeLoader } = require('@craco/craco')
 const PolyfillIPFSScriptSrc = require('./PolyfillIPFSScriptSrc.js')
 const IpfsWebpackPlugin = require('./index.js')
-const base64Loader = require('base64-inline-loader')
+const base64Loader = require('./craco-base64-inline-loader.js')
 
 module.exports = {
     overrideCracoConfig: ({ cracoConfig, pluginOptions, context: { env, paths } }) => { 
+      cracoConfig.plugins.push(
+      { 
+        plugin: base64Loader,
+        options: {
+          test: /\.(ttf|eot|otf|svg|woff(2)?)$/i,
+          limit: 99999999999999999999,
+        }
+      })
       return cracoConfig
     },
     overrideWebpackConfig: ({ webpackConfig, cracoConfig, pluginOptions, context: { env, paths } }) => {
-      let loader = 'base64-inline-loader?limit=99999999999999999999'
-      const base64Loader = {
-        test: /\.(ttf|eot|otf|svg|woff(2)?)$/i,
-        use: loader
-      }
-      addBeforeLoader(webpackConfig, loaderByName('file-loader'), base64Loader)
-
       webpackConfig.plugins.unshift(new PolyfillIPFSScriptSrc())
       webpackConfig.plugins.push(new IpfsWebpackPlugin())
 
