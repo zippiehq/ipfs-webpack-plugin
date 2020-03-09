68403660f69302dd8d912952280fccbb6171ed20
Tom <tom@zippie.org>
2020-03-09 09:37:54 +0000

    Migrating to @zippie/ipfs-bridge
    Signed-off-by: Tom <tom@zippie.org>

--- postmsg-proxy-stub.js
+++ postmsg-proxy-stub.js
@@ -1,42 +1,37 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import { createProxyClient } from "@zippie/ipfs-postmsg-proxy";
+import IpfsBridgeClient from "@zippie/ipfs-bridge/src/client";
 import { BrotliDecompressBuffer as decompress } from "@zippie/brotli/dec/decode";
-
-if (!window.ipfs) {
-  window.ipfs = createProxyClient({
-    postMessage: function (message, origin) {
-      return window.parent.postMessage(message, origin)
-    }
-  })
-}
 
 window.brotli_decompress = function (content) {
   return Buffer.from(decompress(content))
 }
 
-window.ipfs_fetch = async function (cid, brotli = false) {
-  if (false) {
-    const chunks = []
-    for await (const chunk of window.ipfs.cat(cid)) {
-      chunks.push(chunk)
-    }
-    const contents = Buffer.concat(chunks)
-  }
+if (!window.ipfs) {
+  window.ipfs = new IpfsBridgeClient()
+  window.ipfs.init().then(() => {
+    console.info("Zippie IPFS bridge client ready.")
 
-  let contents = await window.ipfs.cat(cid)
-
-  let decompressed = contents
-  if (brotli) {
-    try {
-      decompressed = Buffer.from(window.brotli_decompress(decompressed))
-      console.log('decompress ok')
-    } catch (err) {
-      console.log(err)
+    window.ipfs_fetch = async function (cid, brotli = false) {
+      const chunks = []
+      for await (const chunk of window.ipfs.cat(cid)) {
+        chunks.push(chunk)
+      }
+      const contents = Buffer.concat(chunks)
+    
+      let decompressed = contents
+      if (brotli) {
+        try {
+          decompressed = Buffer.from(window.brotli_decompress(decompressed))
+          console.log('decompress ok')
+        } catch (err) {
+          console.log(err)
+        }
+      }
+      return decompressed
     }
-  }
-  return decompressed
+    
+    window.ipfs_stub_loaded = true
+  })
 }
-
-window.ipfs_stub_loaded = true
